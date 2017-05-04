37bc3eebb6a9b6390da1d555eadef62d87f503d7
Igor Geldiev <xanderakk@mail.ru>
2017-05-04 14:26:49 +0300

    commit

--- src/main/java/com/apress/spring/RestController/JournalRest.java
+++ src/main/java/com/apress/spring/RestController/JournalRest.java
@@ -25,7 +25,7 @@ public class JournalRest {
             list = service.findAll();
         return list;
     }
-    @RequestMapping("/ins")
+    @RequestMapping("/insertdata")
     public String home() throws ParseException {
         service.insertData();
         return "Data inserted succesfully";
